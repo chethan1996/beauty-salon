4815a797a4fd4e58666225e2bb0021275a9b8027
Chethan M <46964474+chethan1996@users.noreply.github.com>
2022-12-26 17:06:11 +0000

    Add logo

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import shineLogo from "../../img/shine_logo.png"
+import Logo from "../../img/logo.jpeg"
 import "./navbar.css"
 
 export default function Navbar() {
@@ -8,7 +8,9 @@ export default function Navbar() {
     <>
     <nav className="navbar navbar-expand-lg navbar-light bg-light">
     <div className="container">
-        <a className="navbar-brand" href="#">Navbar</a>
+        <a className="navbar-brand" href="#">
+          <img src={Logo} className="figure-img img-fluid rounded" alt="Ready To Shine" width={200}/>
+        </a>
         <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <span className="navbar-toggler-icon"></span>
         </button>
