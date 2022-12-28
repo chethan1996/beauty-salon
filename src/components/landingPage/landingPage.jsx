1e623367e9feb629c643a0121f347ae51d26f53d
Chethan M <46964474+chethan1996@users.noreply.github.com>
2022-12-28 11:44:45 +0000

    Add font size

--- src/components/landingPage/landingPage.jsx
+++ src/components/landingPage/landingPage.jsx
@@ -5,8 +5,8 @@ export default function LandingPage(){
 
     return (
         <>
-            <p>
-            <h1 className="title is-1">Welcome to <span className="animate-charcter">Ready 2 Shine</span> </h1>
+            <p className="font-style">
+            <span className="">Welcome to <strong className="animate-charcter">READY TO SHINE</strong> </span>
             </p>
         
         </>
