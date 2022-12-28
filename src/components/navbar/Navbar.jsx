56c4974931eddc35dc78495493e89a95068e407a
Chethan M <46964474+chethan1996@users.noreply.github.com>
2022-12-28 15:21:13 +0000

    Add navbar cross css

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -3,15 +3,24 @@ import Logo from "../../img/logo.jpeg"
 import "./navbar.css"
 
 export default function Navbar() {
+function addClass(){
+  document.querySelector('.animated-icon1').classList.toggle('open');
+}
 //   const [isActive, setisActive] = React.useState(false);
   return (
     <>
     <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
   <div className="container-fluid">
     <a className="navbar-brand" href="#"></a>
-    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+    {/* <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
       <span className="navbar-toggler-icon"></span>
-    </button>
+    </button> */}
+
+    <button className="navbar-toggler first-button" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
+       aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation" onClick={addClass}>
+        <div className="animated-icon1"><span></span><span></span><span></span></div>
+      </button>
+
     <div className="collapse navbar-collapse" id="navbarNav">
       <ul className="navbar-nav ms-md-auto gap-2">
         <li className="nav-item rounded">
