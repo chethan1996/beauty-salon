4b3730907dd49aca07865db05502f760c2467eb1
Chethan M <46964474+chethan1996@users.noreply.github.com>
2022-12-28 11:02:45 +0000

    Add new navbar

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -3,31 +3,40 @@ import Logo from "../../img/logo.jpeg"
 import "./navbar.css"
 
 export default function Navbar() {
-  const [isActive, setisActive] = React.useState(false);
+//   const [isActive, setisActive] = React.useState(false);
   return (
     <>
-    <nav className="navbar navbar-expand-lg navbar-light bg-light">
-    <div className="container">
-        <a className="navbar-brand" href="#">
-          <img src={Logo} className="figure-img img-fluid rounded" alt="Ready To Shine" width={200}/>
-        </a>
-        <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-            <span className="navbar-toggler-icon"></span>
-        </button>
-        <div className="collapse navbar-collapse" id="navbarSupportedContent">
-            <ul className="navbar-nav me-auto">
-                <li className="nav-item active">
-                    <a className="nav-link" href="#" data-bs-toggle="collapse" data-bs-target=".navbar-collapse.show">Services</a>
-                </li>
-                <li className="nav-item">
-                    <a className="nav-link" href="#" data-bs-toggle="collapse" data-bs-target=".navbar-collapse.show">Gallery</a>
-                </li>
-                <li className="nav-item">
-                    <a className="nav-link" href="#" data-bs-toggle="collapse" data-bs-target=".navbar-collapse.show">Contact Us</a>
-                </li>
-            </ul>
-        </div>
+    <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+  <div className="container-fluid">
+    <a className="navbar-brand" href="#"></a>
+    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+      <span className="navbar-toggler-icon"></span>
+    </button>
+    <div className="collapse navbar-collapse" id="navbarNav">
+      <ul className="navbar-nav ms-md-auto gap-2">
+        <li className="nav-item rounded">
+          <a className="nav-link active" aria-current="page" href="#"><i className="bi bi-house-fill me-2"></i>Home</a>
+        </li>
+        <li className="nav-item rounded">
+          <a className="nav-link" href="#"><i className="bi bi-people-fill me-2"></i>About</a>
+        </li>
+        <li className="nav-item rounded">
+          <a className="nav-link" href="#"><i className="bi bi-telephone-fill me-2"></i>Contact</a>
+        </li>
+        <li className="nav-item dropdown rounded">
+          <a className="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false"><i className="bi bi-person-fill me-2"></i>Profile</a>
+          <ul className="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdown">
+            <li><a className="dropdown-item" href="#">Account</a></li>
+            <li><a className="dropdown-item" href="#">Another action</a></li>
+            <li>
+              <hr className="dropdown-divider" />
+            </li>
+            <li><a className="dropdown-item" href="#">Logout</a></li>
+          </ul>
+        </li>
+      </ul>
     </div>
+  </div>
 </nav>
 
 </>
